e5a54ba5f88d9b1ad2ff3c3d117b95d11788ddf8
Jose E Matute <60519538+josematute@users.noreply.github.com>
2024-04-08 15:11:53 -0600

    Update 2gltf2.sh

--- 2gltf2.sh
+++ 2gltf2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [ "$#" = 1 ]
 then
-	blender -b -P 2gltf2.py -- "$1"
+	# blender -b -P 2gltf2.py -- "$1"
+	blender -b -P /opt/2gltf2/2gltf2.py -- "$1" # for docker
 else
 	echo To glTF 2.0 converter.
 	echo Supported file formats: .abc .blend .dae .fbx. .obj .ply .stl .usd .wrl .x3d
