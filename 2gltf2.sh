f17f249397e0f8033dd6d98c5a1ac3feed246cb8
Jose E Matute <60519538+josematute@users.noreply.github.com>
2024-04-08 14:17:49 -0600

    Update 2gltf2.sh

--- 2gltf2.sh
+++ 2gltf2.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-if [ "$#" = 1 ]
-then
-	blender -b -P 2gltf2.py -- "$1"
-else
-	echo To glTF 2.0 converter.
-	echo Supported file formats: .abc .blend .dae .fbx. .obj .ply .stl .usd .wrl .x3d
-	echo  
-	echo 2gltf2.sh [filename]
-fi
+# docker-entrypoint.sh
+
+# Forward all arguments to the 2gltf2.sh script
+/opt/2gltf2/2gltf2.sh "$@"
